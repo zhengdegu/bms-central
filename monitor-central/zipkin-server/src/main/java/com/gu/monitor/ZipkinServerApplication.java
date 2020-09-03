9d8650c822ce54dfbf59fad2cde96366a41da9d1
guzhende <guzhende@ict.ac.cn>
2020-09-03 16:28:29 +0800

    添加zipkin

---
+++ monitor-central/zipkin-server/src/main/java/com/gu/monitor/ZipkinServerApplication.java
@@ -0,0 +1,17 @@
+package com.gu.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
+
+/**
+ * @author FastG
+ * @date 2020/9/3 16:24
+ */
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
+}
